3979d90741ce864b200d14d33c23b0068e5fbe52
EauDeRo <157409734+EauDeRo@users.noreply.github.com>
2024-05-08 17:48:08 +0200

    Add files via upload

--- 02_heladeria.py
+++ 02_heladeria.py
@@ -14,5 +14,33 @@ elif int(num_bolas) < 0:
 else:
     print("¡Buena ellección!")
 
+sabores_de_helado=[]
+
+for i in range(0,int(num_bolas)):
+    añadir_sabor=input("Dime el sabor un numero " + str(i+1) +": ")
+    print(añadir_sabor + "!! ¡¡Qué buena elección!!")
+    sabores_de_helado.append(añadir_sabor)
+
+print("\n\n ¡Perfecto!, recapitulemos, querías un/a " + contenedor +" de helado de "+num_bolas +" bolas.\n Los sabores que has elegido son los siguientes:  " + sabores_de_helado.__str__() )
+
+print ("aquí lo tienes: \n")
+
+
+if int(num_bolas)== 0: dibubolas="   \n"
+elif int(num_bolas) == 1: dibubolas="  0\n"
+elif int(num_bolas) == 2 : dibubolas="  0\n  0\n"
+else : dibubolas=" 000\n"
+
+if contenedor == "cucurucho" :
+    dibuenvase=" \\ /\n  V\n"
+    print(dibubolas + dibuenvase )
+elif contenedor == "tarrina" :
+    dibuenvase=" |___|\n"
+    print(dibubolas + dibuenvase )
+else : print(dibubolas + contenedor +"\n")
+
+print(" Espero que te haya gustado y recuerda nuestros helados son digitales y por lo tanto  tienen 0 calorias . Además, nuestros helados son aptos para todo tipos de alergias e intolerancias :) \n ")
+
+
 
 
