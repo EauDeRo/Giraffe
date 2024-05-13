cbdab9685a6a097e9a3b36a71031b32eca2c9183
EauDeRo <157409734+EauDeRo@users.noreply.github.com>
2024-05-13 11:43:29 +0200

    Add files via upload

--- 06_acceso_archivos_txt.py
+++ 06_acceso_archivos_txt.py
@@ -1,19 +1,23 @@
-#Este programa funciona si se encuentra en la carpeta C:\Users\Rocío\Desktop\PYTHON\ROG\curso
-#Crea un fichero en C:\Users\Rocío\Desktop\PYTHON\ROG
-txt_hi =open("ROG/hi.txt","w")
-txt_hi.write("¡Hola!etoy borrando y escribiendo en un fichero txt")
+#la carpeta main es C:\Users\rocio.guichoux\Desktop\curso python\curso freecodecamp\Giraffe
+# el .py en la carpeta C:\Users\rocio.guichoux\Desktop\curso python\curso freecodecamp\Giraffe\06_acceso_archivos_txt
+#Crea un fichero en C:\Users\rocio.guichoux\Desktop\curso python\curso freecodecamp\Giraffe\ficheros
+txt_hi =open("ficheros/hi.txt","w")
+txt_hi.write("¡Hola!estoy creando un fichero nuevo o  borrando y escribiendo en un fichero hi.txt existente")
 txt_hi.close()
 
-txt_hi_append = open("ROG/hi.txt","a")
+txt_hi_append = open("ficheros/hi.txt","a")
 txt_hi_append.write("\n Esta es la segunda linea del fichero que he añadido luego")
 txt_hi_append.close()
 
 
-txt_hi_read = open("ROG/hi.txt","r")
+txt_hi_read = open("ficheros/hi.txt","r")
+print(txt_hi_read.readlines())
+txt_hi_read.close()
 
-for line in txt_hi_read.readlines():
+
+txt_hi_read2 = open("ficheros/hi.txt","r")
+for line in txt_hi_read2.readlines():
     print("\n leyendo for   " + line)
 
-print(txt_hi_read.read())
-
+print(txt_hi_read2.read())
 txt_hi_read.close()
