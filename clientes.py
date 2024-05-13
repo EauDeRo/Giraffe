7c3e68bdefcbb6cbbca4926b30fed022fb2690bb
EauDeRo <157409734+EauDeRo@users.noreply.github.com>
2024-05-13 18:00:34 +0200

    Add files via upload

---
+++ clientes.py
@@ -0,0 +1,12 @@
+class Cliente:
+    
+    def __init__(self,name,ftp,user,pasword):
+        self.name =name
+        self.ftp =ftp
+        self.user = user
+        self.pasword =pasword
+
+
+
+
+
